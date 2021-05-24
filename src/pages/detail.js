670f34da3eb4e649068166abc260892047fe47ae
kjjoo <vkdlfl0224@naver.com>
2021-05-24 20:21:17 +0900

    Solve Error

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/commons/header";
-import { fetchItem } from "../services";
+// import { fetchItem } from "../services";
 
 export default function Detail() {
     const { id } = useParams();
