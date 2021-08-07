71da9adc5f4a256150fce894444c1f0adcc5a6a5
yceffort <yceffort@gmail.com>
2021-08-07 20:52:54 +0900

    change setting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,10 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    proxy('/api', {
-      target: 'http://15.165.169.129/',
+    '/api',
+    createProxyMiddleware({
+      target: 'http://15.165.169.129',
       changeOrigin: true,
     }),
   )
